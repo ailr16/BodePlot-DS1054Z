22d49acc878ff1209e8d6d59f4cc129502359e1d
ailr16 <angel_ivan_5@outlook.com>
2021-06-22 17:43:09 -0500

    avance2

--- BodePlot.py
+++ BodePlot.py
@@ -1,6 +1,11 @@
 import pyvisa
 import feeltech
+import time
+import numpy
+import matplotlib.pyplot as plt
+import math
 
+timeDelay = 0.6
 startFreq = float(input('Start Frequency (in Hz): '))
 endFreq = float(input('End Frequency (in Hz): '))
 if startFreq < 0 or endFreq < 0:
@@ -35,7 +40,7 @@ def showParameters():
 	print('Frequency Steps=', freqSteps)
 	print('Frequency Increment=', freqInc)
 
-freqInc = (endFreq/freqSteps)
+freqInc = ((endFreq-startFreq)/freqSteps)
 #showParameters()
 
 #Init Scope
@@ -43,17 +48,45 @@ rm = pyvisa.ResourceManager()
 print(rm.list_resources())
 instrument = input('Please enter the oscilloscope ID: ')
 scope = rm.open_resource(instrument)
-
+scope.write("MEASure:CLEar ALL")
+scope.write("MEASure:ITEM VMAX,CHANnel1")
+scope.write("MEASure:ITEM VMAX,CHANnel2")
 
 #Init Gen and CH1
 ft = feeltech.FeelTech('/dev/ttyUSB0')
 c1 = feeltech.Channel(1,ft)
 
-c1.frequency(endFreq)
+CH1VMax = numpy.zeros(freqSteps+1)
+CH2VMax = numpy.zeros(freqSteps+1)
+db = numpy.zeros(freqSteps+1)
+freqValues = numpy.zeros(freqSteps+1)
 c1.amplitude(waveVMax*2)
-#scope.write("MEASure:ITEM VMAX,CHANnel1")
-vmax = scope.query("MEASure:ITEM? VMAX,CHANnel1")
-print('VMAX=',vmax)
+freq = startFreq
+c1.frequency(freq)
+time.sleep(2*timeDelay)
+i = 0
+while i <= freqSteps:
+	CH1VMax[i] = scope.query("MEASure:ITEM? VMAX,CHANnel1")
+	CH2VMax[i] = scope.query("MEASure:ITEM? VMAX,CHANnel2")
+	freqValues[i] =freq;
+	freq = freq + freqInc
+	c1.frequency(freq)
+	time.sleep(timeDelay)
+	i = i + 1
+
+print(CH2VMax)
+print(freqValues)
+
+
+db = (CH2VMax/CH1VMax)
+db = numpy.log10(db)
+
+plt.plot(freqValues,db)
+plt.xlabel('f')
+plt.ylabel('dB')
+plt.title('Bode Plot')
+plt.grid()
+plt.show()
 
 scope.close()
 
