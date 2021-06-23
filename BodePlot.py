dedebe5fe3b187da576a1a1068538bd4c2103577
ailr16 <angel_ivan_5@outlook.com>
2021-06-23 16:55:53 -0500

    Working :-)

--- BodePlot.py
+++ BodePlot.py
@@ -5,9 +5,12 @@ import numpy
 import matplotlib.pyplot as plt
 import math
 
-timeDelay = 0.7
-startFreq = float(input('Start Frequency (in Hz): '))
-endFreq = float(input('End Frequency (in Hz): '))
+########################
+timeDelay = 0.7 #Adjust the time delay between frequency increments (in seconds)
+########################
+
+startFreq = float(input('Start Frequency (in Hz): '))	#Read the start frequency
+endFreq = float(input('End Frequency (in Hz): '))		#Read the end frequency
 if startFreq < 0 or endFreq < 0:
 	print('ERROR. Frequency must be possitive')
 	print('Please press Enter to exit :-(')
@@ -19,76 +22,79 @@ if startFreq > endFreq:
 	input()
 	exit()
 
-waveVMax = float(input('Sine Wave Max Voltage: '))
-if waveVMax <= 0:
-	print('ERROR. Max Voltage must be greater than zero')
-	print('Please press Enter to exit :-(')
-	input()
-	exit()
-		
-freqSteps = int(input('Frequency Steps: '))
+freqSteps = int(input('Frequency Steps: '))			#Read the frequency steps
 if freqSteps <= 0:
 	print('ERROR. Max Voltage must be greater than zero')
 	print('Please press Enter to exit :-(')
 	input()
 	exit()
+waveVMax = float(input('Sine Wave Max Voltage: '))		#Read the max voltage of sine wave
+if waveVMax <= 0:
+	print('ERROR. Max Voltage must be greater than zero')
+	print('Please press Enter to exit :-(')
+	input()
+	exit()
 
-def showParameters():
-	print('Start Frequency=', startFreq, "Hz")
-	print('End Frequency=', endFreq, "Hz")
-	print('Sine Wave Max Voltage=', waveVMax, "V")
-	print('Frequency Steps=', freqSteps)
-	print('Frequency Increment=', freqInc)
+freqInc = ((endFreq-startFreq)/freqSteps)			#Compute the frequency increments in function of steps, start and end frequencies
 
-freqInc = ((endFreq-startFreq)/freqSteps)
-#showParameters()
+rm = pyvisa.ResourceManager()					#PyVISA Resource Manager
+print("List of connected instruments:")
+print(rm.list_resources())					#Show the list of detected instruments
+instrument = input('Please enter the oscilloscope ID: ')	#Read the ID of oscilloscope
+scope = rm.open_resource(instrument)				#Identify oscilloscope with "scope"
+scope.write("MEASure:CLEar ALL")				#Clear all measurement items
+scope.write("MEASure:ITEM VMAX,CHANnel1")			#Create the VMax measurement item for CH1
+scope.write("MEASure:ITEM VMAX,CHANnel2")			#Create the VMax measurement item for CH2
 
-#Init Scope
-rm = pyvisa.ResourceManager()
-print(rm.list_resources())
-instrument = input('Please enter the oscilloscope ID: ')
-scope = rm.open_resource(instrument)
-scope.write("MEASure:CLEar ALL")
-scope.write("MEASure:ITEM VMAX,CHANnel1")
-scope.write("MEASure:ITEM VMAX,CHANnel2")
+ft = feeltech.FeelTech('/dev/ttyUSB0')			#Connect the FY3224s generator
+c1 = feeltech.Channel(1,ft)					#Init the CH1 of generator
 
-#Init Gen and CH1
-ft = feeltech.FeelTech('/dev/ttyUSB0')
-c1 = feeltech.Channel(1,ft)
+CH1VMax = numpy.zeros(freqSteps+1)				#Create an array for CH1 measurements
+CH2VMax = numpy.zeros(freqSteps+1)				#Create an array for CH2 measurements
+db = numpy.zeros(freqSteps+1)					#Create an array for the result in db
+freqValues = numpy.zeros(freqSteps+1)				#Create an arrayo for values of frequency
 
-CH1VMax = numpy.zeros(freqSteps+1)
-CH2VMax = numpy.zeros(freqSteps+1)
-db = numpy.zeros(freqSteps+1)
-freqValues = numpy.zeros(freqSteps+1)
-c1.amplitude(waveVMax*2)
+c1.waveform(feeltech.SINE)					#CH1 will generate a sine wave
+c1.amplitude(waveVMax*2)					#Set CH1 peak to peak voltage
 freq = startFreq
-c1.frequency(freq)
-scope.write("TIMebase:MAIN:SCAle " + str(1/(3*freq)))
-time.sleep(2*timeDelay)
+c1.frequency(freq)						#Set CH1 frequency
+
+scope.write("TIMebase:MAIN:SCAle " + str(1/(3*freq)))	#Set horizontal scale of oscilloscope
+
+if waveVMax <= 3.5:						#Set vertical scale of oscilloscope
+	scope.write("CHANnel1:SCALe 1")
+	scope.write("CHANnel2:SCALe 1")
+	
+elif waveVMax > 3.5 and waveVMax <= 7:
+	scope.write("CHANnel1:SCALe 2")
+	scope.write("CHANnel2:SCALe 2")
+	
+elif waveVMax > 7:
+	scope.write("CHANnel1:SCALe 5")
+	scope.write("CHANnel2:SCALe 5")
+	
+time.sleep(2*timeDelay)					#Time delay
+
 i = 0
 while i <= freqSteps:
-	CH1VMax[i] = scope.query("MEASure:ITEM? VMAX,CHANnel1")
-	CH2VMax[i] = scope.query("MEASure:ITEM? VMAX,CHANnel2")
-	freqValues[i] = freq;
-	freq = freq + freqInc
-	c1.frequency(freq)
-	scope.write("TIMebase:MAIN:SCAle "+ str(1/(3*freq)))
-	time.sleep(timeDelay)
-	i = i + 1
+	c1.frequency(freq)						#Set CH1 (gen) frequency 
+	scope.write("TIMebase:MAIN:SCAle "+ str(1/(3*freq)))		#Set the horizontal scale of oscilloscope
+	time.sleep(timeDelay)						#Time delay
+	CH1VMax[i] = scope.query("MEASure:ITEM? VMAX,CHANnel1")	#Read and save CH1 VMax
+	CH2VMax[i] = scope.query("MEASure:ITEM? VMAX,CHANnel2")	#Read and save CH2 Vmax
+	freqValues[i] = freq;						#Save actual frequency
+	freq = freq + freqInc						#Increment frequency
+	i = i + 1							#Increment index
 
-print(CH2VMax)
-print(freqValues)
+db = (CH2VMax/CH1VMax)				#Cocient between CH2VMax and CH1VMax (for compute db)
+db = 20*numpy.log10(db)			#Compute db
 
-
-db = (CH2VMax/CH1VMax)
-db = numpy.log10(db)
-
-plt.plot(freqValues,db)
+plt.plot(freqValues,db)			#Graph data
 plt.xlabel('f')
 plt.ylabel('dB')
 plt.title('Bode Plot')
 plt.grid()
 plt.show()
 
-scope.close()
+scope.close()					#Stop communication with oscilloscope
 
