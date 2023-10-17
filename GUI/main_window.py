56b8b16a47db32d443b6dd0ffe342e4b997e488d
Angel Lozano <angel_ivan_5@outlook.com>
2023-10-17 17:17:56 -0600

    Finished TestFrame structure

--- GUI/main_window.py
+++ GUI/main_window.py
@@ -6,7 +6,7 @@ class Colors():
     def _rgb_to_tkHex(rgb):
         return "#%02x%02x%02x" % rgb
     
-    MAIN_WINDOW_BG  = _rgb_to_tkHex((242, 242, 242))
+    MAIN_WINDOW_BG  = _rgb_to_tkHex((240, 240, 240))
     FRAME_BG        = _rgb_to_tkHex((227, 227, 227))
 
 
@@ -32,7 +32,7 @@ class InstrumentFrame(tk.Frame):
         self.config(border=1)
         self.config(bg=Colors.FRAME_BG)
         
-        self.grid(row=0, column=0, sticky="nsew")
+        self.grid(row=0, column=0, sticky="nsew", pady=4, padx=10)
 
         # label instrument
         self.label_name_frame = tk.Label(self, text='INSTRUMENT')
@@ -62,12 +62,53 @@ class TestFrame(tk.Frame):
         self.config(border=1)
         self.config(bg=Colors.FRAME_BG)
         
-        self.grid(row=1, column=0, sticky="nsew")
+        self.grid(row=1, column=0, sticky="nsew", pady=4, padx=10)
 
         # label instrument
-        self.label_name_frame = tk.Label(self, text='INSTRUMENT')
+        self.label_name_frame = tk.Label(self, text='TEST')
         self.label_name_frame.config(bg=Colors.FRAME_BG)
-        self.label_name_frame.pack(side="top", anchor="w")
+        self.label_name_frame.grid(row=0, column=0, sticky="w")
+
+        # label start frequency
+        self.label_startFrequency = tk.Label(self, text='Start Frequency (Hz)')
+        self.label_startFrequency.config(bg=Colors.FRAME_BG)
+        self.label_startFrequency.grid(row=1, column=0, sticky="w")
+
+        # textbox start frequency
+        self.textbox_startFrequency = tk.Text(self)
+        self.textbox_startFrequency.config(width=24, height=1)
+        self.textbox_startFrequency.grid(row=1, column=1, sticky="w")
+
+        # label end frequency
+        self.label_endFrequency = tk.Label(self, text='End Frequency (Hz)')
+        self.label_endFrequency.config(bg=Colors.FRAME_BG)
+        self.label_endFrequency.grid(row=2, column=0, sticky="w")
+
+        # textbox end frequency
+        self.textbox_endFrequency = tk.Text(self)
+        self.textbox_endFrequency.config(width=24, height=1)
+        self.textbox_endFrequency.grid(row=2, column=1, sticky="w")
+
+        # label frequency steps
+        self.label_frequencySteps = tk.Label(self, text='Frequency Steps')
+        self.label_frequencySteps.config(bg=Colors.FRAME_BG)
+        self.label_frequencySteps.grid(row=3, column=0, sticky="w")
+
+        # textbox frequency steps
+        self.textbox_frequencySteps = tk.Text(self)
+        self.textbox_frequencySteps.config(width=24, height=1)
+        self.textbox_frequencySteps.grid(row=3, column=1, sticky="w")
+
+        # label max voltage
+        self.label_maxVoltage = tk.Label(self, text='Max Voltage (V)')
+        self.label_maxVoltage.config(bg=Colors.FRAME_BG)
+        self.label_maxVoltage.grid(row=4, column=0, sticky="w")
+
+        # textbox max voltage
+        self.textbox_maxVoltage = tk.Text(self)
+        self.textbox_maxVoltage.config(width=24, height=1)
+        self.textbox_maxVoltage.grid(row=4, column=1, sticky="w")
+
 
 
 class BodePlotterApp(tk.Tk):
