e543722141b15185ad2c6dbc83836750d8751a94
Angel Lozano <angel_ivan_5@outlook.com>
2023-11-18 02:10:37 -0600

    Documented main script

--- bodePlot.py
+++ bodePlot.py
@@ -1,8 +1,11 @@
 from GUI.main_window import MainWindow
 from GUI.main_window import BodePlotterApp
 
-
+# Instantiate a main window
 window_app = MainWindow()
 
+# Instantiate the app (with required frames)
 app = BodePlotterApp(window_app)
+
+# Run the GUI
 app.main_window.mainloop()
